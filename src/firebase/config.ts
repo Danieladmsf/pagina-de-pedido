38d622ade3e84b7b06f6c40fb1d5717286a3066e
DanieL Rodrigues <danielopbinarias@gmail.com>
2026-04-14 16:33:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2243391254-75492",
+  "appId": "1:1044409297961:web:5b5d1509a9bddc8019c038",
+  "apiKey": "AIzaSyAVes6z9Na9FpGkxtq-1HyD9ufrkumHYtA",
+  "authDomain": "studio-2243391254-75492.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1044409297961"
+};
